27075b04962d6d5df20863eeab6a3860a5526e2f
Stanslous <stancemuindi21@gmail.com>
2022-08-25 06:19:44 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Stance is the champion";
+
+
+
+
+
